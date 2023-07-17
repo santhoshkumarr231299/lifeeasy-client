41302b96a82231f737e36f03616a6805d76e62d0
santhoshkumar.r <santhoshkumar.r@gofrugal.com>
2023-07-17 22:43:19 +0530

    added execute sql for superadmin

--- src/modules/SuperAdmin/Screen/ExecuteSql.tsx
+++ src/modules/SuperAdmin/Screen/ExecuteSql.tsx
@@ -3,7 +3,10 @@ import axios from "../../../api/axios";
 import { useNavigate } from "react-router-dom";
 
 function ExecuteSql() {
+  const [columns, setColumns] = useState<string[]>([]);
+  const [datas, setDatas] = useState<any[]>([]);
   const [query, setQuery] = useState<string>("");
+  let counter = 0;
 
   const navigate = useNavigate();
 
@@ -13,26 +16,42 @@ function ExecuteSql() {
         query: query,
       })
       .then((resp) => {
-          console.log(resp);
-      }).catch((error) => {
-          navigate("/login");
+        setDatas(resp.data.data);
+        setColumns([]);
+        if (resp.data.data && resp.data.data.length > 0) {
+          Object.entries(resp.data.data[0]).forEach((element) => {
+            setColumns((prev) => [...prev, element[0]]);
+          });
+        }
+      })
+      .catch((error) => {
+        navigate("/login");
       });
   };
 
-  const bodyStyle: any = {
-    
-  };
+  const bodyStyle: any = {};
 
-  const textAreaStyle = {
+  const textAreaStyle: any = {
     marginLeft: "20px",
-    marginTop : "20px"
+    marginTop: "20px",
   };
 
-  const buttonStyle = {
+  const buttonStyle: any = {
     marginLeft: "20px",
   };
 
-  const dataStyle = {};
+  const dataStyle: any = {
+    backgroundColor: "white",
+    border: "",
+  };
+
+  const tableBorderStyle: any = {
+    border: "1px solid",
+  };
+
+  const tableInnerTextStyle: any = {
+    margin: "5px",
+  };
 
   return (
     <div style={bodyStyle}>
@@ -45,7 +64,28 @@ function ExecuteSql() {
       <button style={buttonStyle} onClick={() => executeSql()}>
         Execute Query
       </button>
-      <div style={dataStyle}></div>
+      <div style={dataStyle}>
+        <table style={tableBorderStyle}>
+          <tr style={tableBorderStyle}>
+            {columns.map((col) => (
+              <td key={col} style={tableBorderStyle}>
+                <div style={tableInnerTextStyle} style={{ fontWeight: "bold" }}>
+                  {col}
+                </div>
+              </td>
+            ))}
+          </tr>
+          {datas.map((data) => (
+            <tr key={counter++} style={tableBorderStyle}>
+              {columns.map((col) => (
+                <td key={col} style={tableBorderStyle}>
+                  <div style={tableInnerTextStyle}>{data[col]}</div>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
