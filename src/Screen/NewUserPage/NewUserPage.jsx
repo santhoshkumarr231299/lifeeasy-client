8619f6c6ad4ba67b12ce0f94bb232ae5717b9dee
santhosh-18782 <santhoshkumar.r@gofrugal.com>
2024-02-15 10:16:03 +0530

    changed the org name to Bussiness name

--- src/Screen/NewUserPage/NewUserPage.jsx
+++ src/Screen/NewUserPage/NewUserPage.jsx
@@ -85,8 +85,8 @@ function NewUserPage() {
     {
       fieldName: "pharmacyname",
       type: "text",
-      labelName: "Bussiness Name",
-      placeholder: "Bussiness Name",
+      labelName: "Business Name",
+      placeholder: "Business Name",
       required: dispProp ? "" : "none",
       reference: pharmacyName,
     },
@@ -415,7 +415,7 @@ function NewUserPage() {
                   onChange={(e) => handleChecked(e)}
                 />
               </div>
-              <div className="org-login">Bussiness account</div>
+              <div className="org-login">Business account</div>
             </div>
           </Form>
         </Card.Body>
